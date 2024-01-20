b737937b2c4b9f42d54539eafa9b61713b603de4
Scott Biggs <scott.biggs88@gmail.com>
2024-01-20 16:33:51 -0500

    [tested] Code to run ring launcher test

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -64,64 +64,26 @@ class Robot : public frc::TimedRobot {
   frc::PowerDistribution                     PDP                   {C_PDP_ID,               frc::PowerDistribution::ModuleType::kRev};
 
   // CAN Motor Controllers
-  // rev::CANSparkMax                           m_frontLeftSteerMotor {frontLeftSteerDeviceID,  rev::CANSparkMax::MotorType::kBrushless};
-  // rev::CANSparkMax                           m_frontLeftDriveMotor {frontLeftDriveDeviceID,  rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax                           m_Motor1 {1,  rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax                           m_Motor2 {2,  rev::CANSparkMax::MotorType::kBrushless};
   // rev::CANSparkMax                           m_frontRightSteerMotor{frontRightSteerDeviceID, rev::CANSparkMax::MotorType::kBrushless};
   // rev::CANSparkMax                           m_frontRightDriveMotor{frontRightDriveDeviceID, rev::CANSparkMax::MotorType::kBrushless};
   // rev::CANSparkMax                           m_rearLeftSteerMotor  {rearLeftSteerDeviceID,   rev::CANSparkMax::MotorType::kBrushless};
   // rev::CANSparkMax                           m_rearLeftDriveMotor  {rearLeftDriveDeviceID,   rev::CANSparkMax::MotorType::kBrushless};
   // rev::CANSparkMax                           m_rearRightSteerMotor {rearRightSteerDeviceID,  rev::CANSparkMax::MotorType::kBrushless};
   // rev::CANSparkMax                           m_rearRightDriveMotor {rearRightDriveDeviceID,  rev::CANSparkMax::MotorType::kBrushless};
-  frc::Spark spark1{0};
-  frc::Spark spark2{1};
-  frc::Spark spark3{2};
-  frc::Spark spark4{3};
+  //frc::Spark spark1{0};
+  //frc::Spark spark2{1};
+  //frc::Spark spark3{2};
+  //frc::Spark spark4{3};
 
-  // rev::SparkMaxPIDController                 m_frontLeftDrivePID    = m_frontLeftDriveMotor.GetPIDController();
-  // rev::SparkMaxPIDController                 m_frontRightDrivePID   = m_frontRightDriveMotor.GetPIDController();
-  // rev::SparkMaxPIDController                 m_rearLeftDrivePID     = m_rearLeftDriveMotor.GetPIDController();
-  // rev::SparkMaxPIDController                 m_rearRightDrivePID    = m_rearRightDriveMotor.GetPIDController();
-  
-  // #ifdef CompBot
-  // rev::CANSparkMax                           m_ArmPivot            {KeMAN_i_ArmPivot,        rev::CANSparkMax::MotorType::kBrushless};
-  // rev::CANSparkMax                           m_Wrist               {KeMAN_i_Wrist,           rev::CANSparkMax::MotorType::kBrushless};        
-  // rev::CANSparkMax                           m_Gripper             {KeMAN_i_Gripper,         rev::CANSparkMax::MotorType::kBrushless};
-  // rev::CANSparkMax                           m_IntakeRollers       {KeINT_i_IntakeRollers,   rev::CANSparkMax::MotorType::kBrushless};
-
-  // rev::SparkMaxPIDController                 m_ArmPivotPID         = m_ArmPivot.GetPIDController();
-  // rev::SparkMaxPIDController                 m_WristPID            = m_Wrist.GetPIDController();
-  // rev::SparkMaxPIDController                 m_GripperPID          = m_Gripper.GetPIDController();
-  // rev::SparkMaxPIDController                 m_IntakeRollersPID    = m_IntakeRollers.GetPIDController();
-
-  // WPI_TalonSRX                               m_TurretRotate         {KeMAN_i_TurretRotate};
-  // WPI_TalonSRX                               m_LinearSlide          {KeMAN_i_LinearSlide};
-
-  // frc::Compressor                            m_pcmCompressor          {KeINT_i_PCM, frc::PneumaticsModuleType::CTREPCM};
-  // frc::Solenoid                              m_PCM_Valve              {KeINT_i_PCM, frc::PneumaticsModuleType::CTREPCM, 0};
-  // #endif
-
-  // // PWM Motor / Light Controllers
-  // #ifdef CompBot
-  // frc::Spark                                 m_vanityLightControler {C_VanityLight_ID};
-  // #endif
+  rev::SparkMaxPIDController                 m_Motor1_PID   = m_Motor1.GetPIDController();
+  rev::SparkMaxPIDController                 m_Motor2_PID   = m_Motor2.GetPIDController();
 
   // // CAN Encoders
-  // rev::SparkMaxRelativeEncoder               m_encoderFrontLeftSteer  = m_frontLeftSteerMotor.GetEncoder();
-  // rev::SparkMaxRelativeEncoder               m_encoderFrontLeftDrive  = m_frontLeftDriveMotor.GetEncoder();
-  // rev::SparkMaxRelativeEncoder               m_encoderFrontRightSteer = m_frontRightSteerMotor.GetEncoder();
-  // rev::SparkMaxRelativeEncoder               m_encoderFrontRightDrive = m_frontRightDriveMotor.GetEncoder();
+  rev::SparkRelativeEncoder               m_Motor1Encoder  = m_Motor1.GetEncoder(rev::SparkRelativeEncoder::Type::kHallSensor,42);
+  rev::SparkRelativeEncoder               m_Motor2Encoder  = m_Motor2.GetEncoder(rev::SparkRelativeEncoder::Type::kHallSensor,42);
 
-  // rev::SparkMaxRelativeEncoder               m_encoderRearLeftSteer   = m_rearLeftSteerMotor.GetEncoder();
-  // rev::SparkMaxRelativeEncoder               m_encoderRearLeftDrive   = m_rearLeftDriveMotor.GetEncoder();
-  // rev::SparkMaxRelativeEncoder               m_encoderRearRightSteer  = m_rearRightSteerMotor.GetEncoder();
-  // rev::SparkMaxRelativeEncoder               m_encoderRearRightDrive  = m_rearRightDriveMotor.GetEncoder();
-
-  // #ifdef CompBot
-  // rev::SparkMaxRelativeEncoder               m_ArmPivotEncoder        = m_ArmPivot.GetEncoder();
-  // rev::SparkMaxRelativeEncoder               m_WristEncoder           = m_Wrist.GetEncoder();
-  // rev::SparkMaxRelativeEncoder               m_GripperEncoder         = m_Gripper.GetEncoder();
-  // rev::SparkMaxRelativeEncoder               m_IntakeRollersEncoder   = m_IntakeRollers.GetEncoder();
-  // #endif
 
   // Driver Inputs
   frc::Joystick c_joyStick{0};
